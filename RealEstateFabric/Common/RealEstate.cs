bdaddc941cc318b653d8c3df24a18e6b991fd7c3
Lesa96 <dragomirovic.sasa.96@gmail.com>
2022-01-17 14:40:27 +0100

    Initial commit with model and RealEstateService & ClientApiService

---
+++ RealEstateFabric/Common/RealEstate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class RealEstate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double Price { get; set; }
+
+        //For historyDb:
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
